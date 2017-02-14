96c0d429aab1663419ad1068d7a181595a14d8f8
Cedric David <cedric.david@btopenworld.com>
2017-02-14 10:36:26 +0000

    Done.

--- spec/count_sentences_spec.rb
+++ spec/count_sentences_spec.rb
@@ -1,32 +1,32 @@
 describe String do
-  describe "#sentence?" do 
-    it "returns true if the string that you are calling this method on ends in a period." do 
+  describe "#sentence?" do
+    it "returns true if the string that you are calling this method on ends in a period." do
       expect("Hi, my name is Sophie.".sentence?).to eq(true)
     end
 
-    it "returns false if the string that you are calling this method on does NOT end in a period." do 
+    it "returns false if the string that you are calling this method on does NOT end in a period." do
       expect("Hi, my name is Sophie".sentence?).to eq(false)
     end
 
   end
 
-  describe "#question?" do 
-    it "returns true if the string that you are calling this method on ends in a question mark." do 
+  describe "#question?" do
+    it "returns true if the string that you are calling this method on ends in a question mark." do
       expect("What's your name?".question?).to eq(true)
     end
 
-    it "returns false if the string that you are calling this method on does NOT end in question mark." do 
+    it "returns false if the string that you are calling this method on does NOT end in question mark." do
       expect("Happy Halloween!".question?).to eq(false)
     end
 
   end
 
-  describe "#exclamation?" do 
-    it "returns true if the string that you are calling this method on ends in an exclamation mark" do 
+  describe "#exclamation?" do
+    it "returns true if the string that you are calling this method on ends in an exclamation mark" do
       expect("Hi, my name is Sophie!".exclamation?).to eq(true)
     end
 
-    it "returns false if the string that you are calling this method on does NOT end in a exclamation mark." do 
+    it "returns false if the string that you are calling this method on does NOT end in a exclamation mark." do
       expect("Hi, my name is Sophie".exclamation?).to eq(false)
     end
 
@@ -43,7 +43,7 @@ describe String do
     end
 
     it "returns the number of sentences in a complex string" do
-      complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+      complex_string = "This, well, is a sentence. This is too! And so is this, I think? Woo."
       expect(complex_string.count_sentences).to eq(4)
     end
   end
